0eb6f3febe42dd2902e2e68963d833c4a0eb442c
LeeShunSheng <1837417085@qq.com>
2021-01-05 14:50:27 +0800

    发现一首歌很好听，塞了进去

--- F-homework/F-work.py
+++ F-homework/F-work.py
@@ -2,6 +2,7 @@
 1 在实际爬取百度热搜时发现存在不同的结果。程序中的热搜可能与百度显示出的热搜存在不同
 2 热搜有时候打不开……不太清楚其中的奥妙
 3 希望每个人都能在帮主页面得到帮助
+4 最近发现一首歌很好听就加进来了
 """
 
 import sqlite3
@@ -98,12 +99,13 @@ def homepage():
     url1 = '<a href="/picture">OSU玩家分布的图片</a><br>'
     url2 = '<a href="/show">OSU玩家分布的表格</a><br>'
     url3 = '<a href="/heat">现在的百度热搜</a><br>'
-    url4 = '<a href="/help">帮助</a><br>'
-    url5 = '<a href="/ScoreChart">OSU得分前十统计表</a><br>'
-    url6 = '<a href="/AverageScorePicture">OSU得分前十平均得分条形图</a><br>'
+    url4 = '<a href="/ScoreChart">OSU得分前十统计表</a><br>'
+    url5 = '<a href="/AverageScorePicture">OSU得分前十平均得分条形图</a><br>'
+    url6 = '<iframe frameborder="1" marginwidth="0" marginheight="0" width=330 height=86 src="https://music.163.com/outchain/player?type=2&id=32958602&auto=1&height=66"></iframe><br>'
+    url7 = '<a href="/help">帮助</a><br>'
     end = '</center></body></html>'
     print("当前正在访问主页")
-    return start+url1+url2+url3+url5+url6+url4+end
+    return start+url1+url2+url3+url4+url5+url6+url7+end
 
 
 @app.route('/help')
